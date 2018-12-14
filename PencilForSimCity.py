e74fc382c46c9a2f161058106d614c3477dfcd97
Modest_ARC <1414463123@qq.com>
2018-12-14 12:09:15 +0800

    WIP!!

--- PencilForSimCity.py
+++ PencilForSimCity.py
@@ -7,7 +7,9 @@ class PencilForSimCity:
     @staticmethod
     def draw_area(screen, building_color, building_pos, building_name, width=0, name_size=20, name_color=(0, 0, 0)):
         Pencil.draw_rect(screen, building_pos, building_color, width=width)
-        Pencil.write_text(screen, building_name, font_pos=[building_pos[0] + name_size * 0.5, building_pos[1] + name_size * 0.5],
+        name_length = len(building_name)
+        Pencil.write_text(screen, building_name, font_pos=[building_pos[0] + building_pos[2] / 2 - (name_length / 2 * 0.6 * name_size),
+                                                           building_pos[1] + building_pos[3] / 2 - name_size * 0.5],
                           font_size=name_size, color=name_color)
 
     @staticmethod
